ed95611de9183c456db9f193bd83b0b5ae8fdd26
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-22 11:19:39 +0530

    Code Commit

---
+++ Sol_Demo/StylesLib/MyStyles/MyOuterResourceDictionary.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StylesLib.MyStyles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyOuterResourceDictionary : ResourceDictionary
+    {
+        public MyOuterResourceDictionary()
+        {
+            InitializeComponent();
+        }
+    }
+}
